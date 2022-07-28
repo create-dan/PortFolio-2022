21516ef5aa71d82df4c998b36db2a531a96d0a46
Dnyaneshwar <dnyaneshwars0611@gmail.com>
2022-07-28 23:40:17 +0530

    changed procfile

--- server/app.js
+++ server/app.js
@@ -49,13 +49,7 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
-  );
-}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server is listening on port ${PORT}`));
